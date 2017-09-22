820269b92897c72899749ce25efc2734a7b7b48c
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-09-22 15:41:17 -0400

    fixed transpose problem

--- bmstparser/src/mstlstm.py
+++ bmstparser/src/mstlstm.py
@@ -105,7 +105,7 @@ class MSTParserLSTM:
                         conll_sentence[modifier + 1].pred_relation = self.irels[max(enumerate(s), key=itemgetter(1))[0]]
                 else:
                     scores = self.__evaluate(conll_sentence)
-                    scores = np.array([s.npvalue().T[0] for s in scores])
+                    scores = np.array([s.npvalue().T[0] for s in scores]).T
                     heads = decoder.parse_proj(scores)
 
                     for entry, head in zip(conll_sentence, heads):

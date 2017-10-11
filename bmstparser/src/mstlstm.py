233f0afe20a85ba4cb0e4532d1f30c5bc24ad664
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-10-11 19:21:55 -0400

    removed the extra initialization for tunable embeddings

--- bmstparser/src/mstlstm.py
+++ bmstparser/src/mstlstm.py
@@ -35,8 +35,6 @@ class MSTParserLSTM:
             self.elookup.init_row(0, [0] * edim)
             for word in external_embedding.keys():
                 self.elookup.init_row(self.evocab[word], external_embedding[word])
-                if word in self.vocab:
-                    self.wlookup.init_row(self.vocab[word], external_embedding[word])
 
             print 'Initialized with pre-trained embedding. Vector dimensions', edim, 'and', len(
                 external_embedding), 'words'

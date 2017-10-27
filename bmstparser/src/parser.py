1f464370b16ee690424fd62521c21eca6aa7f00f
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-10-27 15:09:15 -0400

    back to 100 steps

--- bmstparser/src/parser.py
+++ bmstparser/src/parser.py
@@ -113,9 +113,9 @@ if __name__ == '__main__':
                     lr = parser.options.lr * 0.75 ** decay_steps
                     parser.trainer.learning_rate = lr
                 closs += loss
-                if t%1==0:
+                if t%10==0:
                     sys.stdout.write('overall progress:' + str(round(100 * float(t) / options.t, 2)) + '% current progress:' + str(round(100 * float(i + 1) / len(mini_batches), 2)) + '% loss=' + str(closs / 10) + ' time: ' + str(time.time() - start) + '\n')
-                    if t%1==0:
+                    if t%100==0:
                         avg_model = mstlstm.MSTParserLSTM(pos, rels, w2i, chars, options, parser)
                         uas, las = test(avg_model, dev_buckets, options.conll_dev, options.output+'/dev.out')
                         print 'dev avg acc', las, uas

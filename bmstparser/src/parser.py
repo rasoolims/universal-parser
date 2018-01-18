62a96353e316721a0afe55ee2bddab5e872c1e16
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2018-01-18 18:31:17 -0500

    turned on eval_non_avg

--- bmstparser/src/parser.py
+++ bmstparser/src/parser.py
@@ -126,17 +126,16 @@ if __name__ == '__main__':
                 closs += loss
                 if t%10==0:
                     sys.stdout.write('overall progress:' + str(round(100 * float(t) / options.t, 2)) + '% current progress:' + str(round(100 * float(i + 1) / len(mini_batches), 2)) + '% loss=' + str(closs / 10) + ' time: ' + str(time.time() - start) + '\n')
-                    if t%100==0:
-                        if options.eval_non_avg:
-                            uas, las = test(parser, dev_buckets, options.conll_dev, options.output + '/dev.out')
-                            print 'dev non-avg acc', las, uas
-                            if las > best_las:
-                                best_las = las
-                                print 'saving non-avg with', best_las, uas
-                                parser.save(options.output + '/model')
-                                no_improvement = 0
-                            else:
-                                no_improvement += 1
+                    if t%10==0:
+                        uas, las = test(parser, dev_buckets, options.conll_dev, options.output + '/dev.out')
+                        print 'dev non-avg acc', las, uas
+                        if las > best_las:
+                            best_las = las
+                            print 'saving non-avg with', best_las, uas
+                            parser.save(options.output + '/model')
+                            no_improvement = 0
+                        else:
+                            no_improvement += 1
                         # avg_model = mstlstm.MSTParserLSTM(universal_tags, rels, chars, options, parser)
                         # uas, las = test(avg_model, dev_buckets, options.conll_dev, options.output+'/dev.out')
                         # print 'dev avg acc', las, uas

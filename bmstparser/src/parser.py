ec2447b7b1c0cf0f801c690ecb0c7c5869f756e4
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2018-01-28 18:01:35 -0500

    added gpu option

--- bmstparser/src/parser.py
+++ bmstparser/src/parser.py
@@ -59,6 +59,8 @@ if __name__ == '__main__':
     parser.add_option("--stop", type="int", dest="stop", default=50)
     parser.add_option("--dynet-mem", type="int", dest="mem", default=0)
     parser.add_option("--dynet-autobatch", type="int", dest="dynet-autobatch", default=0)
+    parser.add_option("--dynet-gpu", action="store_true", dest="--dynet-gpu", default=False,
+                      help='Use GPU instead of cpu.')
 
     (options, args) = parser.parse_args()
     print options
